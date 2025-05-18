4a2eaf947f9e235255c8f73b0f32b9007c7bf221
Charlie <45036245+charliemday@users.noreply.github.com>
2025-05-18 16:14:58 +0100

    Update transcription.py
    Replaced `BaseModel` with `dict`

--- src/groq/types/audio/transcription.py
+++ src/groq/types/audio/transcription.py
@@ -5,16 +5,8 @@ from typing import List, Union
 
 __all__ = ["Transcription"]
 
-class Word(BaseModel):
-    word: str
-    """The transcribed word"""
-    start: float
-    """The start of the word (in milliseconds)"""
-    end: float
-    """The end of the word (in milliseconds)"""
-
 class Transcription(BaseModel):
     text: str
     """The transcribed text."""
-    words: Union[List[Word], None]
+    words: Union[List[dict], None]
     """The words in the transcription depending on the granularity setting."""
