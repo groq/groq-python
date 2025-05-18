a4c1f5febd21673f5f07a69d5c80bfe497e3ea34
Charlie <45036245+charliemday@users.noreply.github.com>
2025-05-18 15:22:55 +0100

    Update transcription.py
    Added the `Word` type to the `Transcription` object

--- src/groq/types/audio/transcription.py
+++ src/groq/types/audio/transcription.py
@@ -1,10 +1,20 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 from ..._models import BaseModel
+from typing import List, Union
 
 __all__ = ["Transcription"]
 
+class Word(BaseModel):
+    word: str
+    """The transcribed word"""
+    start: float
+    """The start of the word (in milliseconds)"""
+    end: float
+    """The end of the word (in milliseconds)"""
 
 class Transcription(BaseModel):
     text: str
     """The transcribed text."""
+    words: Union[List[Word], None]
+    """The words in the transcription depending on the granularity setting."""
